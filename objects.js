fcaffada3c84fff8b53a55eb3f2b0e226c134398
sanjaykumar-cse <111355256+sanjaykumar-cse@users.noreply.github.com>
2022-10-19 15:46:08 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,11 @@
+const personobj = {
+    name: 'sanjay' ,
+    age: 22,
+    dob: 'yyyy-mm-dd',
+    origin: 'asian',
+    nationality: "indian" 
+  }
+  
+  console.log(personobj.name,personobj.age);
+
+  console.log(personobj.age,personobj.name);
